0299f5ca333b28900d790c3d9441df281fa433f7
Rogerio Menezes de Almeida - pesqcol <magno_costa@id.uff.br>
2025-02-14 11:17:37 -0200

    corrections masternode

--- src/simulations/simulation_paris_masternode2.sh
+++ src/simulations/simulation_paris_masternode2.sh
@@ -39,12 +39,20 @@ do
         do
                 for B in 0.1 0.0001
                 do
-                        $exec_python simulation_paris.py 100000 $lc $R $B
+                        $exec_python simulation_paris.py 10000 $lc $R $B
 
-                       cat sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed1-CR.txt >../neutrinos/results/sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc-CR.txt
-                        rm  dados_nitro/dadosNitro_radius_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed1.txt
-                        sed -i -e '/^#/d' sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed*
-                        cat sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed* >>../neutrinos/results/sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc-CR.txt
+                       	cat sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed1-CR.txt >../neutrinos/results/sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc-CR.txt
+                       	cat sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed1-NU.txt >../neutrinos/results/sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc-NU.txt
+
+                        rm  dados_nitro/dadosNitro_radius_${R}_Mpc_${B}nG_lc_${lc}Mpc_seed1-CR.txt
+                        sed -i -e '/^#/d' sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_seed*-CR*
+                        
+			rm  dados_nitro/dadosNitro_radius_${R}_Mpc_${B}nG_lc_${lc}Mpc_seed1-NU.txt
+                        sed -i -e '/^#/d' sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_seed*-NU*
+
+                        cat sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed*-CR* >>../neutrinos/results/sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc-CR.txt
+                        cat sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc_one_sphere_seed*-NU* >>../neutrinos/results/sim-A_55_Z_26_R_${R}_Mpc_${B}nG_lc_${lc}Mpc-NU.txt
+
                         rm *.txt
                 done
         done
